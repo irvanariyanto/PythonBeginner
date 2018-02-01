6691494cb14de83028db1460e149978bc58bc7d0
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2018-02-01 22:18:50 +0700

    Iteration

---
+++ Iteration.py
@@ -0,0 +1,17 @@
+def Main():
+    for i in range(5):
+        print "Hello"
+    words = ['cat', 'bat', 'hat', 'rat', 'sat']
+
+    for word in words:
+        print word
+
+    num = 0
+    while (num <= 0):
+        num = int(raw_input("Please enter a positive integer: "))
+
+    while True:
+        print "Hello"
+
+if __name__ == "__main__":
+    Main()
