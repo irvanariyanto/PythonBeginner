72365d5763493da729d16f62ed0f6ee0172046b7
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2018-02-01 22:03:14 +0700

    Selection

---
+++ Selection.py
@@ -0,0 +1,10 @@
+num = int(raw_input("Please enter a number: "))
+
+if (num > 0):
+    print "The number is more then zero"
+elif (num < 0):
+    print "The number is less than zero"
+else:
+    print "The number is zero"
+    
+print num
